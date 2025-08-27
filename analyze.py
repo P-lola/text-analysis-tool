ad2ab10cbdf492b28a7f4011a69644d7757c445f
Ilesanmi Pipelola <pipelolailesanmi@gmail.com>
2025-08-27 21:25:53 +0100

    learnt on functions and implemented changes

--- analyze.py
+++ analyze.py
@@ -1,9 +1,19 @@
-print("Welcome to the text analysis tool. \nI will mine and analyze a body of text in a file you give me")
+# Welcome User
+def welcomeUser():
+    print("Welcome to the text analysis tool. " \
+    "\nI will mine and analyze a body of text in a file you give me")
 
-#Get Username
-
-#Get input from user into the terminal
-username = input("To begin, please enter your username:\n")
+# Get Username
+def getUsername():
+    # Get input from user into the terminal
+    usernameFrominput = input("To begin, please enter your username:\n")
+    return usernameFrominput
 
 #Greet the user
-print("Hello " + username + "!")
+def greetUser(name):
+    print("Hello " + username + "!")
+
+
+welcomeUser()
+username = getUsername()
+greetUser(username)
