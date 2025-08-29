b5526f7d9da7da2d6847823fb56b60941e61dbe7
Ilesanmi Pipelola <pipelolailesanmi@gmail.com>
2025-08-29 09:50:42 +0100

    added username validation

--- analyze.py
+++ analyze.py
@@ -1,3 +1,5 @@
+from random_username.generate import generate_username
+
 # Welcome User
 def welcomeUser():
     print("Welcome to the text analysis tool. " \
@@ -7,11 +9,18 @@ def welcomeUser():
 def getUsername():
     # Get input from user into the terminal
     usernameFrominput = input("To begin, please enter your username:\n")
+
+    if (len(usernameFrominput) < 4) or (not usernameFrominput.isidentifier()):
+        print("Your username must be at least 4 characters long, alphanumeric only,\nhave no spaces, and cannot start with a symbol")
+        usernameFrominput = generate_username()[0]
+        print(usernameFrominput)
+        print("Assigning new username...")
+
     return usernameFrominput
 
 #Greet the user
 def greetUser(name):
-    print("Hello " + username + "!")
+    print("Hello " + name + "!")
 
 
 welcomeUser()
