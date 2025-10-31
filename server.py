98f56b6a2644e3307b73f457b9267b3f72926836
Ilesanmi Pipelola <pipelolailesanmi@gmail.com>
2025-10-31 13:16:44 +0100

    Added text-analysis route POST handler

--- server.py
+++ server.py
@@ -1,17 +1,18 @@
-from flask import Flask, render_template, abort
+from flask import Flask, render_template, abort, request
 from stockAnalyze import getCompanyStockInfo
+from analyze import analyzeText
 
 app = Flask(__name__)
 
 # Debug setting set to true
 app.debug = True
 
-@app.route('/health')
+@app.route('/health', methods=["GET"])
 def healthCheck():
     return "Flask server is up and running"
 
 
-@app.route('/analyze-stock/<ticker>')
+@app.route('/analyze-stock/<ticker>', methods=["GET"])
 def analyzeStock(ticker):
     if len(ticker) > 5 or not ticker.isidentifier():
         abort(400, "Invalid ticker symbol")
@@ -21,6 +22,14 @@ def analyzeStock(ticker):
         abort(404, e)
     except:
         abort(500, "Something went wrong running the stock analysis")
+    return analysis
+
+@app.route('/analyze-text', methods=["POST"])
+def analyzeTextHandler():
+    data = request.get_json()
+    if "text" not in data or not data["text"]:
+        abort(400, "No text provided to analyse")
+    analysis = analyzeText("")
     return analysis
 
 if __name__ == '__main__':
